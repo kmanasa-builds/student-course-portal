821efe318af8121342e87c5442ddd22db34f27e6
kmanasa-builds <kondapallimanasa22@gmail.com>
2026-06-14 19:40:18 +0530

    Completed Hands-On 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,20 @@ import { CourseList } from './pages/course-list/course-list';
 import { StudentProfile } from './pages/student-profile/student-profile';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'courses', component: CourseList },
-  { path: 'profile', component: StudentProfile }
+
+  {
+    path: '',
+    component: Home
+  },
+
+  {
+    path: 'courses',
+    component: CourseList
+  },
+
+  {
+    path: 'profile',
+    component: StudentProfile
+  }
+
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+
+  // [property] is one-way binding (Component → DOM)
+  // [(ngModel)] is two-way binding (DOM ↔ Component)
+
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+
+  searchTerm = '';
+
+  availableCourses = 0;
+
+  ngOnInit() {
+    this.availableCourses = 12;
+    console.log('HomeComponent initialised — courses loaded');
+  }
+
+  ngOnDestroy() {
+    console.log('HomeComponent destroyed');
+  }
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
+}
