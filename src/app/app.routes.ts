16b17660602d7278bcb0a790b107eae37a1ba0fd
kmanasa-builds <kondapallimanasa22@gmail.com>
2026-06-16 19:00:29 +0530

    Completed Hands-On 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { CourseList } from './pages/course-list/course-list';
 import { StudentProfile } from './pages/student-profile/student-profile';
+import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
 
 export const routes: Routes = [
 
@@ -18,6 +19,11 @@ export const routes: Routes = [
   {
     path: 'profile',
     component: StudentProfile
+  },
+
+  {
+    path: 'enroll',
+    component: EnrollmentForm
   }
 
 ];
